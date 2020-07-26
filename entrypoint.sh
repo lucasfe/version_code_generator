871918d6d5ed8db16c1c3d45df8b45f200cf01e8
Lucas Ferreira <lucasfe@lucass-mbp.lan>
2020-07-26 20:33:06 -0300

    t

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,9 @@ source=${SOURCE:-.}
 cd ${GITHUB_WORKSPACE}/${source}
 
 count=$(git rev-list --count HEAD)
+echo "Current count is ${count}"
+echo "Current offset is ${1}"
+
 result=`expr $count + $1`
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 echo "Current branch is ${current_branch}"
